87c9c8f20121065d238d2be24a2e29cc864c7b60
Joe Crick <joseph.crick@hotmail.com>
2020-06-05 21:31:41 +0200

    task/Search Movies (#3)
    Add Search component.
    
    Reportd-by: Joe Crick
    
    Co-authored-by: Joe Crick <joseph.crick@finreach.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
-import { Header } from "./components/Header";
+import React, { useState } from "react";
+import { Header } from "./components/Header/Header";
+import { Search } from "./components/Search/Search";
+import { getMovies } from "./api/api";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  async function runSearch(searchQuery) {
+    const movies = await getMovies(searchQuery);
+    setMovies(movies);
+  }
+
   return (
     <div>
       <Header />
+      <Search runSearch={runSearch} />
+      {movies.map((movie) => (
+        <div>{movie.Title}</div>
+      ))}
     </div>
   );
 }
